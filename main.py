34458568cf350acb3104b0909ebb5243621f6a91
9zfan <105131110+9zfan@users.noreply.github.com>
2022-07-04 15:08:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import discord, asyncio
+from os import system
+import subprocess
+from discord.ext import commands
+
+print("""tu mama es puta jaja""")
+
+client = discord.Client()
+
+def murder(cmd):
+    subprocess.call(cmd, shell=False)
+
+@client.event
+async def on_ready():
+
+    width = shutil.get_terminal_size().columns
+ 
+    def ui():
+        print("auth user: {0} ".format(client.user))
+        print()
+    ui() 
+ 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        commands = []
+        z = 0
+        for index, a in enumerate(message.content):
+            if a == " ":
+                commands.append(message.content[z:index])
+                z = index + 1
+        commands.append(message.content[z:])
+        channel = message.channel
+
+        if commands[0] == '#':
+                    if len(commands) == 1:
+                        async for msg in channel.history(limit=9999):
+                            if msg.author == client.user:
+                                try:
+                                    await msg.delete()
+                                except Exception as x:
+                                    pass
+
+        if commands[0] == "#o":
+                await message.delete()
+                await client.change_presence(status=discord.Status.dnd)
+
+        if commands[0] == "#c":
+                await message.delete()
+                await client.change_presence(status=discord.Status.online)
+
+        if commands[0] == "#i":
+                await message.delete()
+                await client.change_presence(status=discord.Status.offline)
+
+        if commands[0] == "#a":
+                await message.delete()
+                await client.change_presence(status=discord.Status.idle) 
+
+                
+              
+client.run(os.environ['token'], bot=False)
